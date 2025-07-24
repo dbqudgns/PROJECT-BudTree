c8a0660b7c0fe287ff1f0ef030609122241602e2
dbqudgns <dbqudgns4632@gmail.com>
2025-07-24 22:52:01 +0900

    setting : post 10개 더미 데이터 생성

--- BACKEND/src/main/java/com/happiness/budtree/util/InitDB.java
+++ BACKEND/src/main/java/com/happiness/budtree/util/InitDB.java
@@ -5,6 +5,8 @@ import com.happiness.budtree.domain.member.Member;
 import com.happiness.budtree.domain.member.Role;
 import com.happiness.budtree.domain.message.Message;
 import com.happiness.budtree.domain.message.SenderType;
+import com.happiness.budtree.domain.post.Emotion;
+import com.happiness.budtree.domain.post.Post;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 @RequiredArgsConstructor
@@ -24,7 +24,8 @@ public class InitDB {
 
     @PostConstruct
     public void init() {
-        initService.dbInit();
+       // initService.dbInit_ChatroomAndMessage();
+        initService.dbInit_Post();
     }
 
     @Component
@@ -34,14 +35,15 @@ public class InitDB {
 
         private final EntityManager em;
         private final BCryptPasswordEncoder bCryptPasswordEncoder;
+        private final ReturnMember returnMember;
 
-        public void dbInit() {
+        public void dbInit_ChatroomAndMessage() {
             Member member = createMember("테스트", "string", "string");
             em.persist(member);
 
             LocalDateTime chatroom = LocalDateTime.of(2025, 1, 1, 0, 0);
 
-            for (int i = 1; i <= 10000; i++) {
+            for (int i = 1; i <= 100000; i++) {
                 LocalDateTime createdDate = chatroom.plusDays(i) // 하루씩 증가
                         .plusHours(i % 24) // 시간 증가
                         .plusMinutes(1)
@@ -50,6 +52,28 @@ public class InitDB {
 
                 makeChatroomAndMessage(member, createdDate);
 
+            }
+
+        }
+
+        public void dbInit_Post() {
+
+            Member member = returnMember.findMemberByUsernameOrTrow("string");
+
+            LocalDateTime post = LocalDateTime.of(2025, 1, 1, 0, 0);
+
+            Emotion[] emotions = Emotion.values();
+
+            for (int i = 1; i <= 99990; i++) {
+                LocalDateTime createdDate = post.plusDays(i)
+                        .plusHours(i % 24)
+                        .plusMinutes(1)
+                        .plusSeconds(10)
+                        .plusNanos(800_000_000);
+
+                Emotion emotion = emotions[(i - 1) % emotions.length]; // emotion 순서대로 할당
+
+                createPost(member, createdDate, emotion);
             }
 
         }
@@ -72,7 +96,7 @@ public class InitDB {
                     .build();
             em.persist(chatroom);
 
-            for (int i = 1; i < 30; i++) {
+            for (int i = 1; i <= 100; i++) {
 
                 SenderType sender = (i % 2 == 0) ? SenderType.MEMBER : SenderType.BUDDY;
                 String content = (sender == SenderType.MEMBER)
@@ -81,13 +105,24 @@ public class InitDB {
 
                 Message message = Message.builder()
                             .chatroom(chatroom)
-                            .senderType(SenderType.BUDDY)
+                            .senderType(sender)
                             .content(content)
                             .createdDate(createdDate.plusSeconds(i))
                             .build();
                 em.persist(message);
 
             }
+        }
+
+        private void createPost(Member member, LocalDateTime createDate, Emotion emotion) {
+            Post post = Post.builder()
+                    .content("안녕하세요. 테스트 진행을 위해 더미 데이터를 만들고 있습니다. 오늘 저는 우울했지만 맛있는 소고기를 먹어 매우 행복합니다.")
+                    .emotion(emotion)
+                    .createdDate(createDate)
+                    .member(member)
+                    .build();
+
+            em.persist(post);
         }
     }
 }
