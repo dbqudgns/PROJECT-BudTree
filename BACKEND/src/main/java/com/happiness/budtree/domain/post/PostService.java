08dc0a9299d21711cf924bc054ab0120f0a10632
dbqudgns <dbqudgns4632@gmail.com>
2025-08-14 01:26:45 +0900

    pref : 일기장 삭제 시 redis 업데이트 수행

--- BACKEND/src/main/java/com/happiness/budtree/domain/post/PostService.java
+++ BACKEND/src/main/java/com/happiness/budtree/domain/post/PostService.java
@@ -20,7 +20,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +31,9 @@ public class PostService {
     private final PostRepository postRepository;
     private final RedisUtil redisUtil;
     private final ReturnMember returnMember;
+
+    private final static String key = "recentSixPost:";
+
 
     @Transactional
     public void createPost(PostRegisterRQ postRegister, CustomMemberDetails customMemberDetails) {
@@ -105,10 +107,11 @@ public class PostService {
     public List<PostEmotionRP> findLatestSixEmotionsByRedis(CustomMemberDetails customMemberDetails) {
 
         String username = customMemberDetails.getUsername();
-        String key = "recentSixPost:" + username;
+
+        String usernameKey = key + username;
 
         // 1. Redis에서 캐시 조회
-        Object cached = redisUtil.getData(key);
+        Object cached = redisUtil.getData(usernameKey);
 
         // 2. 캐시가 존재하고 타입이 올바르면 반환
         if (cached instanceof List<?> cachedList &&
@@ -129,7 +132,7 @@ public class PostService {
         }
 
         // 4. Redis에 캐시 저장 (TTL 10분)
-        redisUtil.setDataExpire(key, res, 60 * 10L);
+        redisUtil.setDataExpire(usernameKey, res, 60 * 10L);
 
         return res;
     }
@@ -225,6 +228,10 @@ public class PostService {
 
     @Transactional
     public void deletePost(Long postId,CustomMemberDetails customMemberDetails){
+
+        String username = customMemberDetails.getUsername();
+        String usernameKey = key + username;
+
         Member member = returnMember.findMemberByUsernameOrTrow(customMemberDetails.getUsername());
         Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("일기장이 존재하지 않습니다."));
 
@@ -232,5 +239,34 @@ public class PostService {
             throw new IllegalArgumentException("일기장 삭제 권한이 없습니다.");
         }
         postRepository.delete(post);
+
+        // 1. Redis에서 캐시 조회
+        Object cached = redisUtil.getData(usernameKey);
+
+        if (cached instanceof List<?>) {
+            List<PostEmotionRP> postList = (List<PostEmotionRP>) cached;
+
+            // 2. 삭제할 postId와 일치하는 데이터 제거
+            boolean removed = postList.removeIf(per -> per.postId().equals(postId));
+
+            if (removed) {
+
+                // 3. 캐시가 없거나 타입이 다르면 DB 조회
+                List<Post> posts = postRepository.findSixLatestPosts(username);
+
+                List<PostEmotionRP> res = new ArrayList<>();
+
+                for (Post p : posts) {
+                    res.add(PostEmotionRP.builder()
+                            .postId(p.getPostId())
+                            .emotion(p.getEmotion())
+                            .build());
+                }
+
+                // 4. Redis에 캐시 저장 (TTL 10분)
+                redisUtil.setDataExpire(usernameKey, res, 60 * 10L);
+            }
+        }
+
     }
 }
