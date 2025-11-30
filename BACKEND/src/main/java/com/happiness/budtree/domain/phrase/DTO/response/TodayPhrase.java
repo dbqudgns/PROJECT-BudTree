3c5ef758e8c522662026b0db974ccdc6e563cfc0
dbqudgns <dbqudgns4632@gmail.com>
2025-11-30 14:39:40 +0900

    pref : 오늘의 글귀 조회 시 좋아요 총 개수 반환

--- BACKEND/src/main/java/com/happiness/budtree/domain/phrase/DTO/response/TodayPhrase.java
+++ BACKEND/src/main/java/com/happiness/budtree/domain/phrase/DTO/response/TodayPhrase.java
@@ -2,6 +2,7 @@ package com.happiness.budtree.domain.phrase.DTO.response;
 
 public record TodayPhrase(
         Long phraseId,
-        String content
+        String content,
+        int likeCount
 ) {
 }
